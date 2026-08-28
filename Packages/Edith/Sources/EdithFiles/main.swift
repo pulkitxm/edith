abdf8ec0feb4d7dae06d7ea0ff430f7b38e9c6bc
Pulkit <kpulkit15234@gmail.com>
2026-08-29 02:41:11 +0530

    Restore Finder with a lightweight launcher

--- Packages/Edith/Sources/EdithFiles/main.swift
+++ Packages/Edith/Sources/EdithFiles/main.swift
@@ -1,3 +1,17 @@
-import Edith
+import Darwin
+import Foundation
 
-EdithFilesApp.main()
+let executable = URL(fileURLWithPath: CommandLine.arguments[0])
+    .deletingLastPathComponent()
+    .appendingPathComponent("../../../../../MacOS/Edith")
+    .standardizedFileURL.path
+let arguments = [executable] + CommandLine.arguments.dropFirst()
+var pointers = arguments.map { strdup($0) } + [nil]
+
+setenv("EDITH_APPLICATION_ROLE", "files", 1)
+_ = pointers.withUnsafeMutableBufferPointer { pointer in
+    executable.withCString { path in
+        execv(path, pointer.baseAddress)
+    }
+}
+exit(127)
