eaeed2401b535bb910b23b65cdfb5b7a4c96240c
Pulkit <kpulkit15234@gmail.com>
2026-08-28 23:24:05 +0530

    feat(text): add Text Utilities settings

--- Packages/Edith/Sources/Edith/Features/Settings/Views/ExtensionsPane.swift
+++ Packages/Edith/Sources/Edith/Features/Settings/Views/ExtensionsPane.swift
@@ -937,7 +937,7 @@ private struct ExtensionDetailRows: View {
             case .calendar: CalendarRows()
             case .notchShelf: NotchShelfRows()
             case .clipboard: ClipboardRows()
-            case .textUtilities: EmptyView()
+            case .textUtilities: TextUtilitiesRows()
             case .focusDim: FocusDimRows()
             case .presenter: PresenterRows()
             case .colorPicker: ColorPickerRows()
