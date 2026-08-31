aab43cb4543a13439317bf8d6644deeac901a348
Pulkit <kpulkit15234@gmail.com>
2026-08-31 07:34:39 +0530

    feat(cli): add database connection creation

--- Packages/Edith/Sources/EdithCLI/DatabaseCLIEnvironment.swift
+++ Packages/Edith/Sources/EdithCLI/DatabaseCLIEnvironment.swift
@@ -9,9 +9,18 @@ public enum DatabaseCLIEnvironment {
     nonisolated(unsafe) public static var runMCPServer: @Sendable () async throws -> Void = {
         try await DatabaseMCPServer().run()
     }
+    nonisolated(unsafe) public static var makeSecretStore:
+        @Sendable () throws -> any DatabaseSecretStore = {
+            try DatabaseKeychainSecretStore()
+        }
+    nonisolated(unsafe) public static var readPassword: @Sendable () throws -> String = {
+        try SecretInput.readFromStdin("database password")
+    }
 
     public static func reset() {
         makeSender = { DatabaseBrokerCommandClient() }
         runMCPServer = { try await DatabaseMCPServer().run() }
+        makeSecretStore = { try DatabaseKeychainSecretStore() }
+        readPassword = { try SecretInput.readFromStdin("database password") }
     }
 }
