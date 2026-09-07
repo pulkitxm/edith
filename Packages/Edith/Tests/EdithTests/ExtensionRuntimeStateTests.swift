9d4b0201dc5db5ce3bdef4c044195bbac70411f9
Pulkit <kpulkit15234@gmail.com>
2026-09-08 03:09:56 +0530

    test: include Text Utilities in disabled detail coverage

--- Packages/Edith/Tests/EdithTests/ExtensionRuntimeStateTests.swift
+++ Packages/Edith/Tests/EdithTests/ExtensionRuntimeStateTests.swift
@@ -274,6 +274,7 @@ import Testing
             ("focusDim", "FocusDimRows", "enabled", "FocusDimRows.swift"),
             ("presenter", "PresenterRows", "presenterEnabled", "PresenterRows.swift"),
             ("colorPicker", "ColorPickerRows", "colorPickerEnabled", "ColorPickerRows.swift"),
+            ("textUtilities", "TextUtilitiesRows", "enabled", "TextUtilitiesRows.swift"),
             ("emoji", "EmojiRows", "emojiEnabled", "EmojiRows.swift"),
             ("homebrew", "HomebrewRows", "enabled", "ExtensionsPane.swift"),
             ("cleaner", "CleanerRows", "enabled", "ExtensionsPane.swift"),
