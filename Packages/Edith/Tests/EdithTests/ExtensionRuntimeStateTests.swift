f63f571658001818f67278ae7676855fb2b86c31
Pulkit <kpulkit15234@gmail.com>
2026-09-08 03:09:57 +0530

    test: include Scratchpad in disabled detail coverage

--- Packages/Edith/Tests/EdithTests/ExtensionRuntimeStateTests.swift
+++ Packages/Edith/Tests/EdithTests/ExtensionRuntimeStateTests.swift
@@ -274,6 +274,7 @@ import Testing
             ("focusDim", "FocusDimRows", "enabled", "FocusDimRows.swift"),
             ("presenter", "PresenterRows", "presenterEnabled", "PresenterRows.swift"),
             ("colorPicker", "ColorPickerRows", "colorPickerEnabled", "ColorPickerRows.swift"),
+            ("scratchpad", "ScratchpadRows", "enabled", "ScratchpadRows.swift"),
             ("emoji", "EmojiRows", "emojiEnabled", "EmojiRows.swift"),
             ("homebrew", "HomebrewRows", "enabled", "ExtensionsPane.swift"),
             ("cleaner", "CleanerRows", "enabled", "ExtensionsPane.swift"),
