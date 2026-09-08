053344a8865d096d3c01e4db4868a6e7109cdc35
Pulkit <kpulkit15234@gmail.com>
2026-09-08 09:52:08 +0530

    fix: align Text Utilities adapter order with the registry

--- Packages/Edith/Sources/EdithKit/Features/Extensions/Services/ExtensionLiveAdapters.swift
+++ Packages/Edith/Sources/EdithKit/Features/Extensions/Services/ExtensionLiveAdapters.swift
@@ -67,10 +67,10 @@ public enum ExtensionLiveAdapters {
     public static let extensionIDs = [
         "usage", "quinjet", "plugins", "appMaintenance", "homebrew", "cleaner", "system",
         "keepAwake", "lidAwake",
-        "systemStats", "micMute", "clipboard", "emoji", "colorPicker", "keystrokeHighlight",
+        "systemStats", "micMute", "clipboard", "textUtilities", "emoji", "colorPicker",
+        "keystrokeHighlight",
         "focusDim", "presenter", "music", "downloads", "notchShelf", "audioMixer", "calendar",
         "attention", "seoAudit",
-        "textUtilities",
     ]
 
     public static func provider(
